fe10385958d4fbe5e34be78bd89ef676f9164537
yarlagaddasaikiran48-netizen <yarlagaddasaikiran48@gmail.com>
2026-01-06 15:05:04 +0530

    Refactor vite.config.js for cleaner syntax

--- eCommerse/vite.config.js
+++ eCommerse/vite.config.js
@@ -1,13 +1,3 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/e-Commerce/',   // ✅ REQUIRED for GitHub Pages
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
+  import react from '@vitejs/plugin-react'
+export default defineConfig({ plugins: [ react({ babel: { plugins: [['babel-plugin-react-compiler']], }, }), ], })
