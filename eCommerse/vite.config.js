460b5864d36c71562a759d96ab07c3d503fdc835
yarlagaddasaikiran48-netizen <yarlagaddasaikiran48@gmail.com>
2026-01-06 14:58:54 +0530

    Set base path for GitHub Pages deployment

--- eCommerse/vite.config.js
+++ eCommerse/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/e-Commerce/',   // ✅ REQUIRED for GitHub Pages
   plugins: [
     react({
       babel: {
